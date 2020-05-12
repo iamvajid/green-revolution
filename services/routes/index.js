d95f367a7fbd45edc714c589c5274416363a2b6f
Abdul Vajid K T <iamvajid@gmail.com>
2020-05-12 23:48:42 +1000

    change declaration to const

--- services/routes/index.js
+++ services/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/user', function(req, res, next) {
+   return res.send({
+     userId: 1,
+     name: 'Test Name',
+     gitHubID: 'iamvajid'
+   })
 });
 
 module.exports = router;
