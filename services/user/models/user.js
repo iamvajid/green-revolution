1353ebba6dabbd91ba7b496e5a094a601c22342e
Abdul Vajid K T <iamvajid@gmail.com>
2020-05-23 23:17:39 +1000

    Adding a user model

---
+++ services/user/models/user.js
@@ -0,0 +1,34 @@
+
+const User = {
+    id: {
+
+    },   
+    name: {
+
+    },
+    avatar: {
+
+    },
+    username: {
+
+    },
+    password: {
+        
+    },
+    lastLogin: {
+
+    },
+    createdAt: {
+
+    },
+    updatedAt: {
+
+    },
+    isActive: {
+
+    }
+
+    
+}
+
+module.exports = User;
